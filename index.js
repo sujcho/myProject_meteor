e01389d407323322487dae06eb7c555ea173ad0f
suicho <chosujin84@gmail.com>
2016-03-25 00:10:25 -0700

    ADD SIGN UP/SIGN IN FEATURE
    - Migrate index2.html to main.html
    - Add useraccount package
    - Add account-google, account-facebook

---
+++ index.js
@@ -0,0 +1,28 @@
+Tasks = new Mongo.Collection("tasks");
+ 
+if (Meteor.isClient) {
+  // This code only runs on the client
+  Template.body.helpers({
+    tasks: function () {
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
+
+  //event listener
+  Template.body.events({
+    //submit any element matches the css selector .new-task
+    "submit .new-task": function(event){
+      event.preventDefault();
+      //get value from form element
+      var text = event.target.text.value;
+
+      Tasks.insert({
+        text: text,
+        createdAt: new Date()
+      });
+
+      //clear form
+      event.target.text.value = "";
+    }
+  })
+}
